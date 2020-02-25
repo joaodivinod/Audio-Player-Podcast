81d207114a4578c3f6d34d332a45fbde5193ea95
João DIvino <joaodivinod@gmail.com>
2020-02-25 14:18:09 -0300

    nova feuture : Quando um audio finaliza, inicia o próximo de uma lista

--- player.js
+++ player.js
@@ -3,22 +3,30 @@ window.player = {
     title: document.querySelector(".card-content h5"),
     artist: document.querySelector(".artist"),
     audio: document.querySelector("audio"),
-    
-        data:{
-        title:
-            "Como começei a programar / Por que criamos a Rocketseat / Nossa Stack",
-        artist: "Diego Fernandes",
-        cover: "files/como-comecei.jpg",
-        file: "files/como-comecei.mp3"
-    },
-    
+        audioData:audios,
+        currrentAdudio:{},
+        currrentPlaying:0,
     start(){
-        this.cover.style.background = `url(${this.data.cover}) no-repeat center center / cover`;
-        this.title.innerText = this.data.title;
-        this.artist.innerText = this.data.artist;
-        this.audio.src = this.data.file
+
+        this.update();
+
+        this.audio.onended = () => this.next();
+        },
+        next(){
+            this.currrentPlaying++
+            this.update()
+            this.audio.play();
+        },
+        update(){
+        this.currrentAdudio = this.audioData[this.currrentPlaying];
+
+        this.cover.style.background = `url(${path(
+            this.currrentAdudio.cover
+            )}) no-repeat center center / cover`;
+        this.title.innerText = this.currrentAdudio.title;
+        this.artist.innerText = this.currrentAdudio.artist;
+        this.audio.src = path(this.currrentAdudio.file)
         }
+    };
     
-    }
-    
-    
+
